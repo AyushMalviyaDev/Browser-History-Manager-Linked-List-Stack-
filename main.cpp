5c9e8b7f6ea06a8fdb3d7bc93262cab5df36843d
Ayush Malviya <ayushmalviya078@gmail.com>
2026-07-31 23:11:28 +0530

    menu like ui

--- main.cpp
+++ main.cpp
@@ -4,16 +4,57 @@ int main()
 {
     BrowserHistory browser;
 
-    browser.visit("google.com");
-    browser.visit("github.com");
-    browser.visit("youtube.com");
-    browser.visit("chatgpt.com");
+    int choice;
+    string url;
 
-    browser.displayHistory();
+    do
+    {
+        cout << "\n===== Browser History Manager =====" << endl;
+        cout << "1. Visit Website" << endl;
+        cout << "2. Go Back" << endl;
+        cout << "3. Go Forward" << endl;
+        cout << "4. Display History" << endl;
+        cout << "5. Delete URL" << endl;
+        cout << "6. Exit" << endl;
 
-    browser.deleteURL("youtube.com");
+        cout << "\nEnter your choice: ";
+        cin >> choice;
 
-    browser.displayHistory();
+        switch(choice)
+        {
+            case 1:
+                cout << "Enter URL: ";
+                cin >> url;
+                browser.visit(url);
+                break;
+
+            case 2:
+                browser.goBack();
+                break;
+
+            case 3:
+                browser.goForward();
+                break;
+
+            case 4:
+                browser.displayHistory();
+                break;
+
+            case 5:
+                cout << "Enter URL to delete: ";
+                cin >> url;
+                browser.deleteURL(url);
+                break;
+
+            case 6:
+                cout << "Thank you for using Browser History Manager!" << endl;
+                break;
+
+            default:
+                cout << "Invalid Choice!" << endl;
+        }
+
+    } while(choice != 6);
 
     return 0;
 }
